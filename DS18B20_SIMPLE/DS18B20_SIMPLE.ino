e7c484eb8cf331fe3da2bf4709f42f7acb983e84
oirfeodent <oirfeodent@users.noreply.github.com>
2016-04-14 00:15:52 +0530

    Added files via upload

---
+++ DS18B20_SIMPLE/DS18B20_SIMPLE.ino
@@ -0,0 +1,62 @@
+const int temperaturePinNumber = 10;
+
+#include <OneWire.h>
+OneWire ds(temperaturePinNumber);
+
+#define DS18S20_ID 0x10
+#define DS18B20_ID 0x28
+float temperature;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool foundTemperature;
+
+  foundTemperature = getTemperature();
+  if ( foundTemperature ) {
+    Serial.println(temperature);
+  } else {
+    Serial.println("Not working");
+  }
+  delay(2000);
+}
+
+boolean getTemperature() {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  //find a device
+  if (!ds.search(addr)) {
+    ds.reset_search();
+    return false;
+  }
+  if (OneWire::crc8( addr, 7) != addr[7]) {
+    return false;
+  }
+  if (addr[0] != DS18S20_ID && addr[0] != DS18B20_ID) {
+    return false;
+  }
+  ds.reset();
+  ds.select(addr);
+  // Start conversion
+  ds.write(0x44, 1);
+  // Wait some time...
+  delay(1000);
+  present = ds.reset();
+  ds.select(addr);
+  // Issue Read scratchpad command
+  ds.write(0xBE);
+  // Receive 9 bytes
+  for ( i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+  // Calculate temperature value
+  temperature = ( (data[1] << 8) + data[0] ) * 0.0625;
+  return true;
+}
+
---
+++ floatSwitch/floatSwitch.ino
@@ -0,0 +1,59 @@
+const byte floatSwitchPin = 8;
+byte oldFloatSwitchState = HIGH;  // assume floatSwitch open because of pull-up resistor
+byte floatSwitchState = HIGH;
+const unsigned long debounceTime = 10;  // milliseconds
+unsigned long floatSwitchPressTime;  // when the floatSwitch last changed state
+
+int timesChanged;
+
+void setup ()
+{
+  Serial.begin (9600);
+  pinMode (floatSwitchPin, INPUT_PULLUP);
+  floatSwitchState = digitalRead (floatSwitchPin);
+  oldFloatSwitchState =  floatSwitchState;
+
+  Serial.println(floatSwitchState);
+  
+  timesChanged = 0;
+}  // end of setup
+
+void loop ()
+{
+  checkFloatSwitch();
+}  // end of loop
+
+void checkFloatSwitch() {
+  // see if floatSwitch is open or closed
+  floatSwitchState = digitalRead (floatSwitchPin);
+
+  // has it changed since last time?
+  if (floatSwitchState != oldFloatSwitchState)
+  {
+    // debounce
+    if (millis () - floatSwitchPressTime >= debounceTime)
+    {
+      floatSwitchPressTime = millis ();  // when we closed the floatSwitch
+      oldFloatSwitchState =  floatSwitchState;  // remember for next time
+      if (floatSwitchState == LOW)
+      {
+        timesChanged++;
+        Serial.print(timesChanged);
+        Serial.print(": ");
+        Serial.print(floatSwitchState);
+        Serial.print(": ");
+        Serial.println ("floatSwitch closed.");
+      }  // end if floatSwitchState is LOW
+      else
+      {
+        timesChanged--;
+        Serial.print(timesChanged);
+        Serial.print(": ");
+        Serial.print(floatSwitchState);
+        Serial.print(": ");
+        Serial.println ("floatSwitch opened.");
+      }  // end if floatSwitchState is HIGH
+    }  // end if debounce time up
+  }  // end of state change
+}
+
